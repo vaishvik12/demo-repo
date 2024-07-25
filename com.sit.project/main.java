4b91fc7fa055cd2e0693240eecbc67b54224e2a8
Vaisvhik <vaisvhikjamalta1@gmail.com>
2024-07-25 20:50:31 +0530

    made change to readme and main.java

--- com.sit.project/main.java
+++ com.sit.project/main.java
@@ -3,6 +3,9 @@ package demo-repo.com.sit.project;
 public class main {
     public static void main(String[] args) {
         System.out.println("just making some changes to github.com");
-        
+        int [] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+        System.out.println("");
+        }
     }
 }
