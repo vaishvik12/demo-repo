e905bd1f8583f0709cd601cb8805639e76958f5e
Vaisvhik <vaisvhikjamalta1@gmail.com>
2024-07-25 21:26:50 +0530

    made a change to java
    wrote a sysout statement

--- com.sit.project/main.java
+++ com.sit.project/main.java
@@ -6,6 +6,8 @@ public class main {
         int [] arr = new int[10];
         for (int i = 0; i < 10; i++) {
         System.out.println("");
+        System.out.println("changes made in first branch");
+
         }
     }
 }
